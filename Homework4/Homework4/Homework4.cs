56dd0530ca6c1608ef68e7462b43c178a3f8d32d
Danife Sucgang <danife.sucgang@ph.softvision.global>
2022-10-20 22:02:10 +0800

    Session 4 Homework

---
+++ Homework4/Homework4/Homework4.cs
@@ -0,0 +1,44 @@
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Homework4
+{
+    [TestClass]
+    public class Homework4
+    {
+
+        private readonly ServiceReference1.CountryInfoServiceSoapTypeClient countryTest =
+            new ServiceReference1.CountryInfoServiceSoapTypeClient(ServiceReference1.CountryInfoServiceSoapTypeClient.EndpointConfiguration.CountryInfoServiceSoap);
+
+        [TestMethod]
+        public void ListofCountryCodes()
+        {
+
+            var result = countryTest.ListOfCountryNamesByCode();
+            var expectedCountryCode = result.OrderBy(x => x.sISOCode);
+            Assert.IsTrue(result.SequenceEqual(expectedCountryCode));
+        }
+
+        [TestMethod]
+        public void ValidateInvalidCountryCode()
+        {
+            var invalidCountryCode = "OOO";
+            var response = countryTest.CountryName(invalidCountryCode);
+            Assert.IsTrue(response.Contains("Country not found in the database"), $"Country code {invalidCountryCode} was found");
+
+        }
+
+        [TestMethod]
+        public void GetLastCountryCode()
+        {
+           
+            var result = countryTest.ListOfCountryNamesByCode();
+            var lastItem = result.Last();
+            var last = countryTest.CountryName(lastItem.sISOCode);
+            Assert.AreEqual(lastItem.sName, last);
+
+        }
+    }
+
+    
+}
